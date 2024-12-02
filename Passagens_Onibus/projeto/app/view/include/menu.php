37f0c168b6207e9dc2b547fa9f7514f7dada0879
Aluno <aluno-lab@ifpr.com>
2024-12-02 15:45:30 -0300

    Alterações no estilo

--- Passagens_Onibus/projeto/app/view/include/menu.php
+++ Passagens_Onibus/projeto/app/view/include/menu.php
@@ -2,14 +2,17 @@
 #Nome do arquivo: view/include/menu.php
 #Objetivo: menu da aplicação para ser incluído em outras páginas
 
+include_once (__DIR__ . "../../estilizacao/menu.css");
+
 $nome = "(Sessão expirada)";
 if (isset($_SESSION[SESSAO_USUARIO_NOME]))
     $nome = $_SESSION[SESSAO_USUARIO_NOME];
 ?>
 <style>
     .navbar {
-        background: linear-gradient(90deg, #09d8df, #054798);
-    }
+        background: linear-gradient(90deg, #0056b3, #007BFF);
+}
+
 </style>
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
     <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
@@ -18,10 +21,16 @@ if (isset($_SESSION[SESSAO_USUARIO_NOME]))
                 <a class="nav-link" href="<?= HOME_PAGE ?>">Home</a>
             </li>
 
+
+            <a class="dropdown-item" href="<?= BASEURL . '/controller/UsuarioController.php?action=list' ?>" style="color: white;">Usuários</a>
+
+
+
+
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
                     role="button" data-toggle="dropdown" aria-haspopup="true"
-                    aria-expanded="false"> Cadastros </a>
+                    aria-expanded="false" style="color: white;"> Cadastros </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item"
                         href="<?= BASEURL . '/controller/UsuarioController.php?action=list' ?>">Usuários</a>
